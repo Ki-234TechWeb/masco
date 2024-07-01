e7880cc4b0a190a9743d245c935c2b89bab667cd
Ki-234TechWeb <165645258+Ki-234TechWeb@users.noreply.github.com>
2024-07-01 02:53:07 +0100

    uploaded my files

---
+++ script.js
@@ -0,0 +1,117 @@
+
+function openNav() {
+    document.getElementById("Menu").style.width = " 440px";
+    document.getElementById("Menu").style.marginRight = " 0";
+
+
+}
+
+function cancel () {
+    document.getElementById("Menu").style.width = " 0";
+    document.getElementById("Menu").style.marginRight = " -50px";
+
+
+
+    
+}
+
+
+const counters= document.querySelectorAll(".counter")
+counters.forEach( (counter) => {
+
+counter.innerText="0"
+
+const updateCounter = () => {
+const target= +counter.getAttribute('data-target');
+const c = +counter.innerText;
+const increment = target / 800;
+
+if ( c < target){
+counter.innerHTML = Math.ceil(c + increment );
+
+setTimeout(updateCounter, 3);
+
+}
+
+else{
+    console.log("done");
+    counter.innerHTML= target ;
+}
+
+
+
+}
+updateCounter();
+
+
+});
+
+function openVid(Event, videoName) {
+var i, videobody ;
+videobody = document.getElementsByClassName("videobody");
+for (i = 0; i < videobody.length; i++) {
+    videobody[i].style.display = "none";
+}
+document.getElementById(videoName).style.display = "flex";
+
+}
+
+//  window.print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
